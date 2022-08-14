3547e8cfba1d56a7aefbb12174fbd4cdc4f750af
Aayush Joshi <aayushj8355@gmail.com>
2022-08-14 10:45:28 +0530

    Program5.c from C instruction completed

--- 2) C Instructions/Program5.c
+++ 2) C Instructions/Program5.c
@@ -1,12 +1,24 @@
-/* If value of an angle is input through the keyboard, write a program to print all its trigonometric angles.
+/* If value of an angle is input through the keyboard, write a program to print all its trigonometric ratios.
  */
 
 #include <stdio.h>
 #include <math.h>
 int main()
 {
-    float angle;
-    printf("Enter the angle:");
+    float angle,sine,cosine,tangent,cotangent,cosecant,secant;
+    printf("Enter the angle: ");
     scanf("%f",&angle);
-
+    sine = sinf(angle);
+    printf("\n sin(%f) = %f",angle,sine);
+    cosine = cosf(angle);
+    printf("\n cos(%f) = %f",angle,cosine);
+    tangent = tanf(angle);
+    printf("\n tan(%f) = %f",angle,tangent);
+    cotangent = 1/tangent;
+    printf("\n cot(%f) = %f",angle,cotangent);
+    cosecant = 1/cosine;
+    printf("\n cosec(%f) = %f",angle,cosecant);
+    secant = 1/sine;
+    printf("\n sec(%f) = %f",angle,secant);
+    return 0;
 }
--- Test.c
+++
@@ -1,4 +0,0 @@
-//
-// Created by Aayus on 14-08-2022.
-//
-
